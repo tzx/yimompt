fde9e7e97685bc3132865901f4346957cf2b0502
Timmy Xiao <timmyxiao1@gmail.com>
2020-01-26 19:23:32 -0500

    Use stdout lock instead of print

--- src/directory.rs
+++ src/directory.rs
@@ -1,7 +1,12 @@
+use std::io::{self, Write};
+
 pub fn display() {
     let current_path = std::env::current_dir().unwrap();
     let shortened_dir = shorten_path(current_path.to_str().unwrap());
-    println!("{}", shortened_dir);
+
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    write!(stdout, "{}", shortened_dir).unwrap();
 }
 
 fn shorten_path(path: &str) -> String {
